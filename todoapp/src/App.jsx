8cbe05b34c15a9ea597ea5cf5c148739f212f770
ikwame <iaagyapong@st.knust.edu.gh>
2024-10-30 21:10:47 +0000

    todo app with react/inline styling

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,12 @@
+import InlineComponent from "./components/InlineComponent";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+     <InlineComponent/>
+    </div>
+  )
+}
+
+export default App;
---
+++ todoapp/src/components/Todoitem.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Todoitem({item}) {
+  return   <h3>{item}</h3>
+}
